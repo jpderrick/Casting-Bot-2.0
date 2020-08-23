053b61a0d50fb29f815379ae1d03ac68b8f22a4c
jpderrick <jpderrick@hotmail.co.uk>
2020-08-23 21:34:26 +0100

    Intial React and npm packages setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained">Hello, World</Button>
+    </div>
+  );
+}
+
+export default App;

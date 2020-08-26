b1f6c1f0f6098d0022b8972a5692614c37c31cff
jpderrick <jpderrick@hotmail.co.uk>
2020-08-26 22:01:33 +0100

    Created Seasons View Template Page

---
+++ src/pages/SeasonViewPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { withStyles } from "@material-ui/core";
+
+const styles = (theme) => ({});
+
+const SeasonViewPage = ({ classes, ...props }) => {
+  return <div>This is the season's view page</div>;
+};
+
+export default withStyles(styles)(SeasonViewPage);

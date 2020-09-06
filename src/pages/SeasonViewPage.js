7d84b0df080b0dd86df51a5b1a25dfd115c675ea
jpderrick <jpderrick@hotmail.co.uk>
2020-09-06 13:56:50 +0100

    Basic Season View page layout

--- src/pages/SeasonViewPage.js
+++ src/pages/SeasonViewPage.js
@@ -1,31 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   withStyles,
+  TableBody,
+  TableRow,
+  Table,
+  TableHead,
+  Typography,
+  TableCell,
+  Paper,
+  IconButton,
   List,
   ListItem,
   ListItemText,
-  Paper,
   ListItemSecondaryAction,
-  Switch,
-  IconButton,
+  Divider,
+  Button,
 } from "@material-ui/core";
-import ChangeShowOrder from "../components/ChangeShowOrder";
+import DeleteIcon from "@material-ui/icons/Delete";
 import SettingsIcon from "@material-ui/icons/Settings";
+import MeetingRoomIcon from "@material-ui/icons/MeetingRoom";
+import NoMeetingRoomIcon from "@material-ui/icons/NoMeetingRoom";
+
 const styles = (theme) => ({
-  showContainer: {
-    border: "1px solid",
-    borderColor: theme.palette.primary.light,
+  seasonList: { marginTop: theme.spacing() },
+  playItem: {
+    marginBottom: theme.spacing(),
   },
 });
 
 const SeasonViewPage = ({ classes, ...props }) => {
+  const [castingOpen, setCastingOpen] = useState(false);
   return (
     <div>
-      <Switch />
       <List>
-        <Paper className={classes.showContainer}>
-          <ListItem>
-            <ListItemText primary="Title of play" />
+        <ListItem>
+          <ListItemText
+            primary={<Typography variant="h6">Season Title</Typography>}
+          />
+          <ListItemSecondaryAction>
+            <Button
+              className={classes.castingCloseOpen}
+              variant="contained"
+              color={castingOpen ? "secondary" : "primary"}
+              onClick={() => setCastingOpen(!castingOpen)}
+              startIcon={
+                castingOpen ? <MeetingRoomIcon /> : <NoMeetingRoomIcon />
+              }
+            >
+              CASTING IS {castingOpen ? "OPEN " : "CLOSED "}
+            </Button>
+          </ListItemSecondaryAction>
+        </ListItem>
+      </List>
+
+      <List className={classes.seasonList}>
+        <Paper>
+          <ListItem className={classes.playItem}>
+            <ListItemText primary="The Drunks" />
+            <ListItemSecondaryAction>
+              <IconButton>
+                <SettingsIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Paper>
+        <Paper>
+          <ListItem className={classes.playItem}>
+            <ListItemText primary="The Drunks" />
             <ListItemSecondaryAction>
               <IconButton>
                 <SettingsIcon />

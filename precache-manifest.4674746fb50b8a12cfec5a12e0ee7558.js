413d03d22211ed7270facb1d59738f30fae7bd83
jpderrick <jpderrick@hotmail.co.uk>
2020-09-20 20:01:16 +0100

    Updates

---
+++ precache-manifest.4674746fb50b8a12cfec5a12e0ee7558.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c66e182d5f0aa22f1a4246a37585874",
+    "url": "/Casting-Bot-2.0/index.html"
+  },
+  {
+    "revision": "6474578463f6d2514c81",
+    "url": "/Casting-Bot-2.0/static/js/2.50c33dc9.chunk.js"
+  },
+  {
+    "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
+    "url": "/Casting-Bot-2.0/static/js/2.50c33dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae01f97ea09d6d4666ff",
+    "url": "/Casting-Bot-2.0/static/js/main.5f2d861b.chunk.js"
+  },
+  {
+    "revision": "8a2894865be6de8c31c0",
+    "url": "/Casting-Bot-2.0/static/js/runtime-main.76fc0bfa.js"
+  }
+]);
